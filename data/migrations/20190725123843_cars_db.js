41c4cf360cd46731f650959c783c870b83395925
hsrsbd <hsraffety@gmail.com>
2019-07-25 12:44:08 -0700

    updating project.

---
+++ data/migrations/20190725123843_cars_db.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+
+    tbl.string('car_make', 255).notNullable();
+    tbl.string('car_model', 255).notNullable();
+    tbl.integer('car_mileage', 128);
+  }
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('cars');
+};
