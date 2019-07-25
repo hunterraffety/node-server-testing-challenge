a9c5f008edd4c69d6801f47752b4b0e7a96813a0
hsrsbd <hsraffety@gmail.com>
2019-07-25 13:02:03 -0700

    basic jest testing implemented.

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,7 +3,17 @@ const request = require('supertest')
 const server = require('./server')
 
 describe('server', () => {
-    it('db environment set to be testing', () => {
-        expect(process.env.DB_ENV).toBe('testing')
+  it('db environment set to be testing', () => {
+    expect(process.env.DB_ENV).toBe('testing')
+  })
+
+  describe('GET /', () => {
+    it('should return 200 OK', () => {
+      return request(server)
+        .get('/')
+        .then(res => {
+          expect(res.status).toBe(200)
+        })
     })
+  })
 })
